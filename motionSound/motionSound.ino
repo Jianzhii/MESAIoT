a6be0760d690fe5c100ef27523ad9ea27544669c
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2022-04-06 14:38:15 +0800

    allow motionSound to send data to camera

--- motionSound/motionSound.ino
+++ motionSound/motionSound.ino
@@ -1,13 +1,27 @@
 // import libraries for WiFI and Http requests
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <espnow.h>
 #include "ThingSpeak.h"
-
 #include "telegram.h"
 
-char* sleepBody = "{\"chat_id\": -1001318460964, \"text\": \"Its sleeping time..\"}";
+typedef struct message {
+   bool sendMsg;
+} message;
+
+message myMessage;
+void onSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Last Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail\n" + String(sendStatus));
+  }
+}
+
 char* motionBody = "{\"chat_id\": -1001318460964, \"text\": \"No motion detected for 5 minute.\"}";
-char* soundBody = "{\"chat_id\": -1001318460964, \"text\": \"Loud sounds have been detected...\"}";
+char* soundBody = "{\"chat_id\": -1001318460964, \"text\": \"Loud sounds have been detected.\"}";
 
 const long utcOffsetInSeconds = 28800;
 
@@ -29,6 +43,28 @@ void setup() {
   
   // connect to Wifi
   WiFi.mode(WIFI_STA);
+  
+  // Get Mac Add
+  Serial.print("Mac Address: ");
+  Serial.print(WiFi.macAddress());
+  Serial.println("ESP-Now Sender");
+  // Initializing the ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Problem during ESP-NOW init");
+    return;
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);  
+  Serial.println("Registering send callback function");
+  esp_now_register_send_cb(onSent);
+  // Register the peer
+  Serial.println("Registering a peer");
+  int addPeer = esp_now_add_peer(broadcastAddress, 2, 3, NULL, 0);
+  if (addPeer == 0) {
+    Serial.println("Successfully registered peer.");
+  } else {
+    Serial.println("Failed to register peer.");
+  }
+
   WiFi.begin(wifiname, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -43,9 +79,9 @@ void loop() {
   timeClient.update();
   Serial.println("############## Motion Check ##############");
   if (timeClient.getHours() > 22 and timeClient.getHours() < 6) {
-    Serial.println("it is morning they sleeping hello");
-    sendTele(sleepBody);
-    motionCounter += 5;
+//    Serial.println("it is night time.");
+//    sendTele(sleepBody);
+//    motionCounter += 5;
   } else {
     motionVal = digitalRead(motionPin);
     Serial.print("Motion Sensor Value: ");
@@ -58,6 +94,7 @@ void loop() {
       motionCounter += 1;
     } while (res != 200);
     Serial.println("Channel update successful.");
+
     
     if (motionVal == 0) {
       motionCounter += 1;
@@ -65,13 +102,15 @@ void loop() {
         motionCounter = 0;
         Serial.println("No motion detected for 5 minute");
         sendTele(motionBody);
+        myMessage.sendMsg = true;
+        esp_now_send(broadcastAddress, (uint8_t *) &myMessage, sizeof(myMessage));
+        delay(5000);
         motionCounter += 5;
       }
     } else {
       motionCounter = 0 ;
     }
   }
-
   Serial.println("############## Sound Check ##############");
   soundVal = analogRead(soundPin);
 
@@ -89,6 +128,9 @@ void loop() {
   if (soundVal > 50) {
     Serial.println("Loud sound detected");
     sendTele(soundBody);
+    myMessage.sendMsg = true;
+    esp_now_send(broadcastAddress, (uint8_t *) &myMessage, sizeof(myMessage));
+    delay(5000);
     motionCounter += 5;
   } else {
     Serial.println("No sound detected");
